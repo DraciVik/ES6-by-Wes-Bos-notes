27c864e9b5f277821f82337163cfbb1e11a814c1
DraciVik <viktor.stojanov90@gmail.com>
2018-07-10 23:33:48 +0200

    Added notes for tagged template literals

--- 03-Template-Strings/template_literals.js
+++ 03-Template-Strings/template_literals.js
@@ -1,67 +1,96 @@
-// // We use backticks "``" for template literals 
-// // With "${}" we can do regular JavaScript inside of template literals.
+// // // We use backticks "``" for template literals 
+// // // With "${}" we can do regular JavaScript inside of template literals.
+
+// // // const markup = `
+// // //     <div>
+// // //         ${person.name}
+// // //         <span class="job">${person.job}</span>
+// // //      <h2>
+// // //       <p class="location>${person.city}</p>
+// // //       <p class="bio">${person.bio}</p>
+// // //     </div>
+// // // `;
+
+// // // document.body.innerHTML = markup;
+
+// // // We can nest template strings inside of eachother 
+
+// // // const dogs = [
+// // //     {name: "Snickers", age: 2},
+// // //     {name: "hobo", age: 8},
+// // //     {name: "Sunny", age: 1}
+// // // ];
+
+// // // const markup = `
+// // //     <ul class="dogs">
+// // //         ${dogs.map(dog => `<li>${dog.name} is ${dog.age * 7}</li>`).join("")}
+// // //     </ul>
+// // // `;
+
+// // // In this case map returns an array meaning that every <li> is separeted with a colon;
+
+// // When we need an if statements we use a ternirary operator. 
 
 // // const markup = `
-// //     <div>
-// //         ${person.name}
-// //         <span class="job">${person.job}</span>
-// //      <h2>
-// //       <p class="location>${person.city}</p>
-// //       <p class="bio">${person.bio}</p>
+// //     <div class="song">
+// //     <p>
+// //         ${song.name} - ${song.artist}
+// //         ${song.featuring ? `(featuring ${song.featuring}` : ""}
+// //         </p>
+// //     </div>
+// // `;
+
+// // When data becomes too complex and we need to refactor we can use the react example - components. 
+
+// // const beer = {
+// //     name: -----,
+// //     brewery: ----
+// //     keywords: [....]
+// // };
+
+// // function renderKeywords(keywords) {
+// //     return `
+// //         <ul>
+// //         ${keywords.map(keyword => `<li>${keyword}</li>`).join("")};
+// //         </ul>
+// //     `;
+// // };
+
+// // const markup = `
+// //     <div class="beer">
+// //     <h2>${beer.name}</h2>
+// //     <p class="brewery">${beer.brewery}</p>
+// //     ${renderKeywords(beer.keywords)}
 // //     </div>
 // // `;
 
 // // document.body.innerHTML = markup;
 
-// // We can nest template strings inside of eachother 
+// TAGGED TEMPLATE LITERALS 
 
-// // const dogs = [
-// //     {name: "Snickers", age: 2},
-// //     {name: "hobo", age: 8},
-// //     {name: "Sunny", age: 1}
-// // ];
+// We can run a template literal trough a function and rather than have the browser put in the variables for us, we can have control of how the string is made. 
 
-// // const markup = `
-// //     <ul class="dogs">
-// //         ${dogs.map(dog => `<li>${dog.name} is ${dog.age * 7}</li>`).join("")}
-// //     </ul>
-// // `;
+// function highlight(strings, ...values) {
+//     let str = "";
+//     strings.forEach((string, i) => {
+//         str += string + (values[i] || "");
+//     });
+//     return str;
+// }
+// or 
 
-// // In this case map returns an array meaning that every <li> is separeted with a colon;
+// function highlight(strings, ...values) {
+//     let str = "";
+//     strings.forEach((string, i) => {
+//         str += `${string} <span class="hl">${values[i] || ""}</span>;`;
+//     });
+//     return str;
+// }
 
-// When we need an if statements we use a ternirary operator. 
+// const name = "snickers";
+// const age = 100;
 
-// const markup = `
-//     <div class="song">
-//     <p>
-//         ${song.name} - ${song.artist}
-//         ${song.featuring ? `(featuring ${song.featuring}` : ""}
-//         </p>
-//     </div>
-// `;
+// const sentence = highlight`My dog's name is ${name} and he is ${age} years old`;
+// document.body.innerHTML = sentence;
 
-// When data becomes too complex and we need to refactor we can use the react example - components. 
-
-// const beer = {
-//     name: -----,
-//     brewery: ----
-//     keywords: [....]
-// };
-
-// function renderKeywords(keywords) {
-//     return `
-//         <ul>
-//         ${keywords.map(keyword => `<li>${keyword}</li>`).join("")};
-//         </ul>
-//     `;
-// };
-
-// const markup = `
-//     <div class="beer">
-//     <h2>${beer.name}</h2>
-//     <p class="brewery">${beer.brewery}</p>
-//     ${renderKeywords(beer.keywords)}
-//     </div>
-// `;
-
-// document.body.innerHTML = markup;
+// We can also use it for when we want to add an abbreviation tag in html
